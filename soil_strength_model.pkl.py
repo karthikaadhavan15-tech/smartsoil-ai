2adabf48a79ed80f8050854b43eb185ef90a8ff9
karthikaadhavan15-tech <karthikaadhavan15@gmail.com>
2025-10-08 22:04:37 +0530

    Delete soil_strength_model.pkl.py

--- soil_strength_model.pkl.py
+++
@@ -1,6 +0,0 @@
-from sklearn.tree import DecisionTreeRegressor
-import joblib
-
-model = DecisionTreeRegressor()
-model.fit(X_train, y_train)  # Your data here
-joblib.dump(model, 'soil_strength_model.pkl')
